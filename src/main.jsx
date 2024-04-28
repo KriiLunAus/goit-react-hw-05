fee3e896f71a811955e7ad429890ff54cffc081b
Krii <kriilunaus16@gmail.com>
2024-04-28 21:07:57 +0200

    normalize

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './components/App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import 'modern-normalize'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
